8d00fc3bf5fe870bbb6043adb7986916e75f512e
catlair <catlair@qq.com>
2022-04-30 22:34:27 +0800

    feat: api https

--- config/proxy.ts
+++ config/proxy.ts
@@ -11,8 +11,9 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/dev-api/': {
       // 要代理的地址
-      target: 'http://localhost:3010/',
+      target: 'https://localhost:3010/',
       // 配置了这个可以从 http 代理到 https
+      secure: false,
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
       // 路径重写
